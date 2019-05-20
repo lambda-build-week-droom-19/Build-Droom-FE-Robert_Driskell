d7b6a28e271e91bfe8e63f33d043767c93d02727
Robert Driskell <driskell.ba@gmail.com>
2019-05-20 13:47:00 -0500

    Fixed Landing Page not loading, filled in some stuff on Seeker and Company profiles

--- droom/src/App.js
+++ droom/src/App.js
@@ -1,55 +1,34 @@
 import React from 'react';
 import './App.css';
 import { fakeAuth } from './utils/axiosWithAuth';
-import { connect } from 'react-redux';
 import {BrowserRouter as Router, Route, Link, Redirect, withRouter} from "react-router-dom"
-import {login} from "./actions/index"
-import SignUpApp from './views/SignUp/SignUpApp';
-/* import LandingPage from './routes/LandingPage';
+import LandingPage from './routes/LandingPage';
 import DebugRouteBobby from './DebugRouteBobby';
-import DebugRouteChase from './DebugRouteChase'; */
+import DebugRouteChase from './DebugRouteChase';
 
-class App extends React.Component {
-    componentWillMount()
-    {
-        fakeAuth.isAuthenticated = localStorage.getItem("userToken") !== "";
-    }
-    render(){
-        return (
-            <div className="App">
-                <AuthButton />
-                    <ul>
-                        <li>
-                            <Link to="/public">Public Page</Link>
-                        </li>
-                        <li>
-                            <Link to="/protected">Protected Page</Link>
-                        </li>
-                    </ul>
-                    <Route path="/public" component={Public} />
-                    <Route path="/login" exact render={(props)=> (<Login {...props} logincb={this.props.login} />)} />
-                    <Route path="/signup" exact render={(props) => (<SignUpApp {...props}/>)} />
-                    <PrivateRoute path="/protected" component={Protected} />
-                    {/* 
-                        Commented out routes for debuging
-                    <Route exact path="/" component={LandingPage} />
-                    <Route exact path="debug-bobby" component={DebugRouteBobby} />
-                    <Route exact path="debug-chase" component={DebugRouteChase} /> 
-                    
-                    */}
-            </div>
-        );
-    }
+function App() {
+    return (
+        <div className="App">
+            <AuthButton />
+                <ul>
+                    <li>
+                        <Link to="/public">Public Page</Link>
+                    </li>
+                    <li>
+                        <Link to="/protected">Protected Page</Link>
+                    </li>
+                </ul>
+                <Route path="/public" component={Public} />
+                <Route path="/login" component={Login} />
+                <PrivateRoute path="/protected" component={Protected} />
+                <Route exact path="/" component={LandingPage} />
+                <Route exact path="/debug-bobby" component={DebugRouteBobby} />
+                <Route exact path="/debug-chase" component={DebugRouteChase} />
+        </div>
+    );
 }
 
-const mapStateToProps = state => 
-{
-    return {
-        ...state
-    }
-}
-
-export default connect(mapStateToProps, {login})(App)
+export default App;
 
 
 const AuthButton = withRouter(
@@ -102,23 +81,24 @@ const AuthButton = withRouter(
 
 
   class Login extends React.Component {
-    state = { redirectToReferrer: false, credentials: {username: "chase", password: "chase123"}};
+    state = { redirectToReferrer: false };
   
     login = () => {
-        fakeAuth.authenticate(this.state.credentials, this.props.logincb, () => this.props.history.push("/protected"));
+      fakeAuth.authenticate(() => {
+        this.setState({ redirectToReferrer: true });
+      });
     };
   
     render() {
       let { from } = this.props.location.state || { from: { pathname: "/" } };
       let { redirectToReferrer } = this.state;
   
-      if (redirectToReferrer || fakeAuth.isAuthenticated) return <Redirect to={from} />;
+      if (redirectToReferrer) return <Redirect to={from} />;
   
       return (
         <div>
           <p>You must log in to view the page at {from.pathname}</p>
           <button onClick={this.login}>Log in</button>
-          <button><Link to="/signup" >Sign Up</Link></button>
         </div>
       );
     }
