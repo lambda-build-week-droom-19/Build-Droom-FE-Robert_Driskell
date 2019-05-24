9420afaea6dac5775f36e08fae59d81061680463
Robert Driskell <driskell.ba@gmail.com>
2019-05-24 00:45:26 -0500

    Styling moved forward

--- droom/src/routes/CompanyAccepted.js
+++ droom/src/routes/CompanyAccepted.js
@@ -1,68 +1,69 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux';
-import { axiosWithAuth } from '../utils/axiosWithAuth';
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { axiosWithAuth } from "../utils/axiosWithAuth";
 
-import { getCurrentUser, updateCurrentUser, getEmployerJobs, SERVER_BASE_URL } from '../actions/index';
+import {
+  getCurrentUser,
+  updateCurrentUser,
+  getEmployerJobs,
+  SERVER_BASE_URL
+} from "../actions/index";
 
 class CurrentCompanyProfile extends Component {
-    state = {
-        people: []
-    }
+  state = {
+    people: []
+  };
 
-    componentDidMount() {
-        this.fetchAccepted();
-    }
+  componentDidMount() {
+    this.fetchAccepted();
+  }
 
-    fetchAccepted = () => {
-        axiosWithAuth()
-            .get(`${SERVER_BASE_URL}/jobs/matches/employer`)
-            .then(res => {
-                console.log(res)
-                const jobsWithAcceptedPeople = res.data.filter(job => {
-                    if (job.usersConfirmed.length > 0) {
-                        return job.usersConfirmed
-                    } else {
-                        return false;
-                    }
-                })
-                console.log(jobsWithAcceptedPeople);
-                const arrayOfArrays = jobsWithAcceptedPeople.map(job => {
-                    console.log(job.usersConfirmed);
-                    return job.usersConfirmed;
-                })
-                console.log(arrayOfArrays);
-                var merged = arrayOfArrays.flat(1);
-                console.log('array');
-                console.log(merged);
-                this.setState({
-                    people: merged
-                    })
-                })
-                console.log('read me')
-                console.log(this.state.jobs);
-            })
-            .catch(err => {
-                console.log(err);
-            })
-    }
+  fetchAccepted = () => {
+    axiosWithAuth()
+      .get(`${SERVER_BASE_URL}/jobs/matches/employer`)
+      .then(res => {
+        console.log(res);
+        const jobsWithAcceptedPeople = res.data.filter(job => {
+          if (job.usersConfirmed.length > 0) {
+            return job.usersConfirmed;
+          } else {
+            return false;
+          }
+        });
+        console.log(jobsWithAcceptedPeople);
+        const arrayOfArrays = jobsWithAcceptedPeople.map(job => {
+          console.log(job.usersConfirmed);
+          return job.usersConfirmed;
+        });
+        console.log(arrayOfArrays);
+        var merged = arrayOfArrays.flat(1);
+        console.log("array");
+        console.log(merged);
+        this.setState({
+          people: merged
+        });
+        console.log("read me");
+        console.log(this.state.jobs);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
 
-    render() {
-        return (
-            <>
-            </>
-        )
-    }
+  render() {
+    return <></>;
+  }
 }
 
 const mapStateToProps = state => {
-    return {
-        company: state.userReducer.currentUser,
-    };
+  return {
+    company: state.userReducer.currentUser
+  };
 };
 
 export default connect(
-    mapStateToProps,
-    {
-        getCurrentUser,
-    }
+  mapStateToProps,
+  {
+    getCurrentUser
+  }
 )(CurrentCompanyProfile);
