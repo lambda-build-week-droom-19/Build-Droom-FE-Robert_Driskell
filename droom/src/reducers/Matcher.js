d46886ba71ecf278fa4a894d79af545db1e27f84
Chase Wex <clapingmanly@hotmail.com>
2019-05-24 01:08:16 -0400

    Matcher funcitonality finalization

--- droom/src/reducers/Matcher.js
+++ droom/src/reducers/Matcher.js
@@ -1,13 +1,17 @@
-import {GET_MATCHES_START, GET_MATCHES_SUCCESS, GET_MATCHES_FAILURE, ACCEPT_MATCH, REJECT_MATCH} from "../actions"
+import {GET_MATCHES_START, GET_MATCHES_SUCCESS, GET_MATCHES_FAILURE, ACCEPT_MATCH, REJECT_MATCH, JOB_ACCEPT_MATCH, JOB_REJECT_MATCH} from "../actions"
 
 
 const initialState = {
     gettingMatch: false,
     currentMatches: [],
+    dontFetch: false,
     error: '',
 }
 
 export const matcher = (state = initialState, action) => {
+    let arry;
+    let index = -1;
+    let indx2 = -1;
     switch (action.type) {
         case GET_MATCHES_START:
             return {
@@ -20,6 +24,7 @@ export const matcher = (state = initialState, action) => {
             return {
                 ...state,
                 gettingMatch: false,
+                dontFetch: action.payload.length,
                 currentMatches: [...state.currentMatches,...action.payload],
                 error: '',
             }
@@ -28,12 +33,48 @@ export const matcher = (state = initialState, action) => {
             return {
                 ...state,
                 gettingMatch: false,
+                dontFetch: true,
                 error: action.payload,
+            }
+        case JOB_ACCEPT_MATCH:
+            arry = state.currentMatches;
+            index = -1;
+            arry.forEach((x,i)=> { if(x.job.id === action.payload.jobid) index = i;})
+            if(index > -1) 
+            {
+                indx2 = -1;
+                arry[index].usersAvailable.forEach((x,i)=> { if(x=== action.payload.user.user_id) indx2=i; })
+                if(index > -1)
+                {
+                    arry[index].usersAvailable.splice(indx2, 1);
+                }
+                if(!arry[index].usersConfirmed.includes(action.payload.user)) arry[index].usersConfirmed.push(action.payload.user);
+            }
+            return{
+                ...state,
+                currentMatches: [...arry]
+            }
+        case JOB_REJECT_MATCH:
+             arry = state.currentMatches;
+            index = -1;
+            arry.forEach((x,i)=> { if(x.job.id === action.payload.jobid) index = i;})
+            if(index > -1) 
+            {
+                indx2 = -1;
+                arry[index].usersAvailable.forEach((x,i)=> { if(x=== action.payload.user.user_id) indx2=i; })
+                if(index > -1)
+                {
+                    arry[index].usersAvailable.splice(indx2, 1);
+                }
+            }
+            return{
+                ...state,
+                currentMatches: [...arry]
             }
 
         case ACCEPT_MATCH:
         case REJECT_MATCH:
-            let arry = state.currentMatches;
+            arry = state.currentMatches;
             arry.shift();
             return {
                 ...state,
